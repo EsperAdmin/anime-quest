c49228d8f72bb827512fa00dc0d79069b14a093b
EsperAdmin <reno@renos.world>
2025-12-26 00:35:14 -0800

    fix stats command not sending and add values

--- bot.py
+++ bot.py
@@ -6,25 +6,34 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# Starting the bot
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print(f"{self.user} has started!")
-
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = MyClient(intents=intents)
-client.run(os.getenv('BOT_TOKEN'))
+# Set command prefix and require intents
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+# Starting the bot
+@bot.event
+async def on_ready():
+    print(f"{bot.user} has started!")
+
+
+# Watch if message is from self
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+    await bot.process_commands(message)
 
 # Commands
-
-@commands.command()
+@bot.command()
 async def stats(ctx):
-    await ctx.send("Here are the bot stats!")
+    await ctx.send(
+    f"**{bot.user.display_name}** stats!"
+    f"\nServers: {len(bot.guilds)}"
+    f"\nLatency: {format(round(bot.latency * 1000))}ms")
 
-# Add commands
-async def setup(bot):
-    bot.add_command(stats)
-    await bot.load_extension('stats')
+# Run the bot and pass ENV Token
+bot.run(os.getenv('BOT_TOKEN'))
+
     
